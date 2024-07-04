924dda7b09178324eb534b4540503968380e0b71
cris24tayler <cristian.tayler@alunoescolassociais.org.br>
2024-07-04 09:04:15 -0300

    adiciona o java script

---
+++ controller.js
@@ -0,0 +1,5 @@
+// validar acesso em tela de login
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+}
