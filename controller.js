b02f0aeafe677fad8054fac588a918127b3cae49
cris24tayler <cristian.tayler@alunoescolassociais.org.br>
2024-07-25 08:49:43 -0300

    cria id para o js no cadastro

--- controller.js
+++ controller.js
@@ -2,4 +2,25 @@
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
+
+    if(!loginEmail || !loginSenha){
+        alert("favor preencher todos os campos")
+    }else{
+        //alert("campos preenchidos com sucesso")
+        window.location.href = 'cadastro.html'
+    }
+}
+
+// função que armazena em array nome na tela de cadastro
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
+    }else{
+        alert("favor informar o nome para cadastro");
+    }
 }
